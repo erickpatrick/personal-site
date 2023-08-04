98798ece645aa1997eb708fb2d63d5e605e0d536
erick patrick <github@erickpatrick.net>
2023-08-04 13:27:15 +0200

    adjusts labels from contact form to ptbr

--- resources/views/livewire/contact.blade.php
+++ resources/views/livewire/contact.blade.php
@@ -44,9 +44,9 @@
         </div>
 
         <div class="mt-6 flex items-center justify-end gap-x-6">
-            <button type="button" class="text-sm font-semibold leading-6 text-gray-900">Cancel</button>
+            <button type="button" class="text-sm font-semibold leading-6 text-gray-900">Cancelar</button>
             <button type="submit" class="rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600">
-                Save
+                Enviar
 
                 <div wire:loading>
                     <svg class="animate-spin -ml-1 mr-3 h-5 w-5 text-white" fill="none" viewBox="0 0 24 24">
