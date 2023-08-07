761647e56259c23675860c027f612ceb2a760781
erick patrick <github@erickpatrick.net>
2023-08-07 20:16:18 +0200

    adjusts to display only seconds if 0min

--- resources/views/components/content-reading-time.blade.php
+++ resources/views/components/content-reading-time.blade.php
@@ -4,4 +4,7 @@
     $minutes = $seconds === 60 ? floor($length / 185) + 1 : floor($length / 185);
 @endphp
 
-<span class="block text-sm leading-6 text-gray-600">{{ $minutes }}min @if($seconds > 0)e {{ $seconds }}s @endif de leitura</span>
+<span class="block text-sm leading-6 text-gray-600">
+    @if($minutes > 0){{ $minutes }}min @endif
+    @if($seconds > 0 && $minutes > 0)e @endif
+    @if($seconds > 0){{ $seconds }}s @endif de leitura</span>
