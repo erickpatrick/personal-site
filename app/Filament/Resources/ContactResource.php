f82050883345f3dd805f31f316faf9f2b8cbce3f
erick patrick <github@erickpatrick.net>
2023-08-13 14:45:05 +0200

    fixes component name for backend view

--- app/Filament/Resources/ContactResource.php
+++ app/Filament/Resources/ContactResource.php
@@ -3,15 +3,12 @@
 namespace App\Filament\Resources;
 
 use App\Filament\Resources\ContactResource\Pages;
-use App\Filament\Resources\ContactResource\RelationManagers;
 use App\Models\Contact;
 use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
 use Filament\Tables\Table;
-use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\SoftDeletingScope;
 
 class ContactResource extends Resource
 {
@@ -27,7 +24,7 @@ class ContactResource extends Resource
                 Forms\Components\TextInput::make('email'),
                 Forms\Components\TextInput::make('subject')
                     ->columnSpanFull(),
-                Forms\Components\TextArea::make('content')
+                Forms\Components\Textarea::make('content')
                     ->columnSpanFull(),
             ]);
     }
