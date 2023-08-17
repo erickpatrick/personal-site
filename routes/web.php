e0a6c23da6b6ab52d2fe6f1ca81b017f264f8e58
erick patrick <github@erickpatrick.net>
2023-08-17 22:47:54 +0200

    adds route to generate sitemap

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,11 @@
 <?php
 
+use Spatie\Sitemap\SitemapGenerator;
+
 Route::feeds();
+
+Route::get('generate-sitemap-site-pessoal', function() {
+    SitemapGenerator::create('https://erickpatrick.net')
+    ->getSitemap()
+    ->writeToFile(public_path('sitemap.xml'));
+});
