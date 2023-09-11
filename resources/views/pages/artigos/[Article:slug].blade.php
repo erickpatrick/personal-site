aa98735a6a65d8bf4367db530e924ea342e961b3
erick patrick <github@erickpatrick.net>
2023-09-12 00:27:13 +0200

    adds anchors to titles for easier sharing

--- resources/views/pages/artigos/[Article:slug].blade.php
+++ resources/views/pages/artigos/[Article:slug].blade.php
@@ -21,7 +21,7 @@ name('articles.view');
                 <small class="text-xl">{{ $article->categories }}</small>
             </div>
             <div class="py-16 lg:py-24">
-                <x-buk-markdown class="prose-lg md:prose-xl lg:prose-2xl" flavor="github">
+                <x-buk-markdown class="prose-lg md:prose-xl lg:prose-2xl" flavor="github" anchors>
                     {!! $article->content !!}
                 </x-buk-markdown>
             </div>
