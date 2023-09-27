e722735f42327585ad2a0f784dffe75783905033
Erick Patrick <github@erickpatrick.net>
2023-09-27 11:12:36 +0200

    Update sitemap to use www
    This should help fixing google search indexing errora

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Spatie\Sitemap\SitemapGenerator;
 Route::feeds();
 
 Route::get('generate-sitemap-site-pessoal', function() {
-    SitemapGenerator::create('https://erickpatrick.net')
+    SitemapGenerator::create('https://www.erickpatrick.net')
     ->getSitemap()
     ->writeToFile(public_path('sitemap.xml'));
 });
