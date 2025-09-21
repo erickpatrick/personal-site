706566271930e4412b94313d9b29144f56f416ec
erick patrick <github@erickpatrick.net>
2025-09-21 23:44:26 +0200

    adds reading time component and view single article page

--- src/app/artigos/[slug]/page.tsx
+++ src/app/artigos/[slug]/page.tsx
@@ -3,6 +3,7 @@ import { getArticleBySlug } from "@/app/artigos/articles";
 import { toLocaleDateString } from "@/app/artigos/locale-date";
 import { notFound } from "next/navigation";
 import markdownToHtml from "@/app/markdownToHtml";
+import ReadingTime from "../reading-time";
 
 export default async function ArticleViewRoot({
   params,
@@ -28,13 +29,13 @@ export default async function ArticleViewRoot({
           {toLocaleDateString(article.date, "pt-BR")}
         </time>
         <span className="block text-sm leading-6 text-gray-600">
-          2min e 20s de leitura
+          <ReadingTime contentLength={content} />
         </span>
       </PageTitle>
       <div className="max-w-5xl mx-auto">
         <div className="py-16 lg:py-24">
           <div
-            className="prose-lg md:prose-xl lg:prose-2xl [&_ul]:list-disc [&_blockquote]:border-l-4 [&_blockquote]:border-blue-300 [&_blockquote]:pl-4 [&_blockquote]:italic [&_pre]:bg-slate-100 [&_code]:bg-slate-100 [&_a]:text-blue-600 [&_a]:hover:underline [&_a]:hover:underline-offset-8 [&_a]:decoration-2 [&_a]:decoration-wavy [&_a]:decoration-blue-600 [&_hr]:bg-slate-200  mx-auto"
+            className="prose-lg md:prose-xl lg:prose-2xl [&_ul]:list-disc [&_blockquote]:overflow-x-auto [&_blockquote]:border-l-4 [&_blockquote]:border-l-blue-300 [&_blockquote_p]:py-4 [&_pre]:overflow-x-auto [&_pre]:bg-blue-50 [&_pre]:shadow-sm [&_code]:break-words [&_code]:bg-blue-50 [&_a]:text-blue-600 [&_a]:hover:underline [&_a]:hover:underline-offset-8 [&_a]:decoration-2 [&_a]:decoration-wavy [&_a]:decoration-blue-600 [&_hr]:bg-slate-200 [&_table]:min-w-full [&_table]:divide-y [&_table]:divide-gray-300 [&_th]:py-3.5 [&_th]:pl-4 [&_th]:pr-3 [&_th]:text-left [&_th]:font-semibold [&_th]:text-gray-900 [&_th]:sm:pl-0 [&_tbody]:divide-y [&_tbody]:divide-gray-200 [&_td]:py-4 [&_td]:pl-4 [&_td]:pr-3 [&_td]:text-gray-900 [&_td]:sm:pl-0 [&_h3]:font-semibold [&_h4]:font-semibold [&_h5]:font-italic mx-auto"
             dangerouslySetInnerHTML={{ __html: content }}
           ></div>
         </div>
