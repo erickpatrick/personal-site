15a5ce1358365c1691a2373a7b05ffa14ab011c5
erick patrick <github@erickpatrick.net>
2025-09-21 19:13:20 +0200

    adds base layout

---
+++ src/components/header.tsx
@@ -0,0 +1,48 @@
+export default function Header() {
+  return (
+    <div className="p-4 flex justify-between align-center">
+      <a href="/" className="group">
+        <div className="flex justify-center">
+          <div className="flex flex-col text-4xl font-black uppercase origin-center -rotate-6 border-4 border-gray-200 group-hover:border-white transition duration-100 ease-out">
+            <h1 className="p-2 bg-transparent text-gray-800 rotate-6 group-hover:bg-gray-800 group-hover:text-white transition duration-200 ease-out delay-100">
+              EP
+            </h1>
+          </div>
+        </div>
+      </a>
+      <nav className="hidden sm:flex py-3 justify-between">
+        <a
+          className="p-2 hover:underline hover:underline-offset-8 decoration-2 decoration-wavy decoration-blue-600 font-bold underline underline-offset-8"
+          href="/"
+        >
+          Inicial
+        </a>
+        <a
+          className="p-2 hover:underline hover:underline-offset-8 decoration-2 decoration-wavy decoration-blue-600"
+          href="/artigos"
+        >
+          Artigos
+        </a>
+        <a
+          className="p-2 hover:underline hover:underline-offset-8 decoration-2 decoration-wavy decoration-blue-600"
+          href="/projetos"
+        >
+          Projetos
+        </a>
+        <a
+          className="p-2 hover:underline hover:underline-offset-8 decoration-2 decoration-wavy decoration-blue-600"
+          href="/contato"
+        >
+          Contato
+        </a>
+      </nav>
+
+      <a
+        className="sm:hidden flex p-4 underline underline-offset-8"
+        href="#footer-menu"
+      >
+        Menu
+      </a>
+    </div>
+  );
+}
