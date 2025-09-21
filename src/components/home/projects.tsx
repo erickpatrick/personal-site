368422f18a85500a66ba8a0a6c430b731624b5a8
erick patrick <github@erickpatrick.net>
2025-09-21 21:58:20 +0200

    adjusts home projects listing to use list instead pure html

--- src/components/home/projects.tsx
+++ src/components/home/projects.tsx
@@ -1,4 +1,22 @@
 export default function Projects() {
+  const porjects = [
+    {
+      name: "RPG do Mestre",
+      top: "RPG do",
+      bottom: "Mestre",
+      description:
+        "Comunidade de RPG de mesa com mais de 100 mil membros. Criação de conteúdo, eventos e suporte para jogadores e mestres.",
+      link: "https://www.rpgdomestre.com/",
+    },
+    {
+      name: "Criador de Cartas",
+      top: "Criador de",
+      bottom: "Cartas",
+      description:
+        "Ferramenta online para criar cartas personalizadas para jogos de RPG, com diversas opções de design e layout.",
+      link: "https://cards.rpgdomestre.com/",
+    },
+  ];
   return (
     <div className="text-center">
       <h3 className="text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl font-black">
@@ -7,36 +25,26 @@ export default function Projects() {
       <div className="bg-white py-8 sm:py-16">
         <div className="mx-auto max-w-7xl px-6 lg:px-8">
           <div className="-mx-6 grid grid-cols-2 gap-0.5 overflow-hidden sm:mx-0 sm:rounded-2xl md:grid-cols-3">
-            <div className="bg-blue-600/5 p-8 sm:p-10">
-              <a
-                href="https://www.rpgdomestre.com/"
-                title="RPG do Mestre"
-                target="_blank"
-                className="text-center"
-              >
-                <span className="inline-block">
-                  <span className="block text-xl font-semibold -mb-2 mt-2">
-                    RPG do
+            {porjects.map((project) => (
+              <div className="bg-blue-600/5 p-8 sm:p-10" key={project.name}>
+                <a
+                  href={project.link}
+                  title={project.name}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="text-center"
+                >
+                  <span className="inline-block">
+                    <span className="block text-xl font-semibold -mb-2 mt-2">
+                      {project.top}
+                    </span>
+                    <span className="block text-2xl font-black">
+                      {project.bottom}
+                    </span>
                   </span>
-                  <span className="block text-2xl font-black">Mestre</span>
-                </span>
-              </a>
-            </div>
-            <div className="bg-blue-600/5 p-6 sm:p-10">
-              <a
-                href="https://cards.rpgdomestre.com/"
-                title="Criador de Cartas"
-                target="_blank"
-                className="text-center"
-              >
-                <span className="inline-block">
-                  <span className="block text-xl font-semibold -mb-2 mt-2">
-                    Criador
-                  </span>
-                  <span className="block text-2xl font-black">de Cartas</span>
-                </span>
-              </a>
-            </div>
+                </a>
+              </div>
+            ))}
           </div>
         </div>
       </div>
