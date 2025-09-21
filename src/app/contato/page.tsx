3d87b4df7f5959219fd6cb61d3ceabd6c4353630
erick patrick <github@erickpatrick.net>
2025-09-21 20:13:10 +0200

    adds page title to contato page and adjust metadata from projetos and contato

--- src/app/contato/page.tsx
+++ src/app/contato/page.tsx
@@ -1,3 +1,18 @@
+import PageTitle from "@/components/page-title";
+
+export const metadata = {
+  title: "Contato",
+  description:
+    "Caso queira contatar-me, aqui estão algumas das formas mais rápidas que você pode usar para entrar em contato comigo a qualquer momento",
+};
+
 export default function ContactsRoot() {
-  return <div>Contacts Root</div>;
+  return (
+    <>
+      <PageTitle
+        title="Contato"
+        description="Caso queira contatar-me, aqui estão algumas das formas mais rápidas que você pode usar para entrar em contato comigo a qualquer momento"
+      />
+    </>
+  );
 }
--- src/app/projetos/page.tsx
+++ src/app/projetos/page.tsx
@@ -1,6 +1,12 @@
 import PageTitle from "@/components/page-title";
 import ProjectsList from "@/components/projetos/projects-list";
 
+export const metadata = {
+  title: "Projetos",
+  description:
+    "Listagem com alguns projetos paralelos em que tenho trabalhado: RPG de Mesa, Vídeo Games, YouTube, e muito mais...",
+};
+
 export default function ProjectsRoot() {
   return (
     <>
