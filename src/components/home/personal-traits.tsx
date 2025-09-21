74da0a19ee5fa64a551083088a66eaaffef0607a
erick patrick <github@erickpatrick.net>
2025-09-21 21:53:21 +0200

    adjusts personal traits to use external svgs

--- src/components/home/personal-traits.tsx
+++ src/components/home/personal-traits.tsx
@@ -1,145 +1,62 @@
+import Image from "next/image";
+
 export default function PersonalTraits() {
+  const traits = [
+    {
+      title: "Brasileiro.",
+      description:
+        "Nascido em Roraima e criado no Piauí, onde construí parte da minha vida. Morei lá até os meus 27 anos.",
+      icon: "globe-alt.svg",
+    },
+    {
+      title: "Poliglota.",
+      description:
+        "Além do Português, também entendo e falo em Inglês (C2), Espanhol (B1), e Alemão (A2/B1).",
+      icon: "language.svg",
+    },
+    {
+      title: "20 anos de experiência.",
+      description:
+        "Comecei na época do Frontpage. Foquei meus trabalhos em sistemas web e mobile.",
+      icon: "sparkles.svg",
+    },
+    {
+      title: "Cientista da Computação.",
+      description:
+        "Formado pela Universidade Estadual do Piauí — UESPI, com muito orgulho.",
+      icon: "academic-cap.svg",
+    },
+    {
+      title: "eCommerce.",
+      description:
+        "Minha carreira na Europa é 100% voltada para criação de grandes lojas online internacionais.",
+      icon: "shopping-bag.svg",
+    },
+    {
+      title: "Arquitetura de Sistemas.",
+      description:
+        "Tenho trabalhado mais na arquitetura e modelagem de sistemas que em programação.",
+      icon: "puzzle-piece.svg",
+    },
+  ];
   return (
     <dl className="mx-auto grid grid-cols-1 gap-x-6 gap-y-10 text-xl leading-7 sm:grid-cols-2 lg:mx-0 lg:max-w-none lg:grid-cols-3 lg:gap-x-8 lg:gap-y-16">
-      <div className="relative pl-9">
-        <dt className="inline font-semibold text-gray-900">
-          <svg
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className="absolute left-1 top-1 h-6 w-6 text-blue-600"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M6.115 5.19l.319 1.913A6 6 0 008.11 10.36L9.75 12l-.387.775c-.217.433-.132.956.21 1.298l1.348 1.348c.21.21.329.497.329.795v1.089c0 .426.24.815.622 1.006l.153.076c.433.217.956.132 1.298-.21l.723-.723a8.7 8.7 0 002.288-4.042 1.087 1.087 0 00-.358-1.099l-1.33-1.108c-.251-.21-.582-.299-.905-.245l-1.17.195a1.125 1.125 0 01-.98-.314l-.295-.295a1.125 1.125 0 010-1.591l.13-.132a1.125 1.125 0 011.3-.21l.603.302a.809.809 0 001.086-1.086L14.25 7.5l1.256-.837a4.5 4.5 0 001.528-1.732l.146-.292M6.115 5.19A9 9 0 1017.18 4.64M6.115 5.19A8.965 8.965 0 0112 3c1.929 0 3.716.607 5.18 1.64"
-            ></path>
-          </svg>
-          Brasileiro.
-        </dt>
+      {traits.map((trait) => (
+        <div className="relative pl-9" key={trait.title}>
+          <dt className="inline font-semibold text-gray-900">
+            <Image
+              src={`/assets/${trait.icon}`}
+              alt={trait.title}
+              width={24}
+              height={24}
+              className="absolute left-1 top-1 h-6 w-6 text-blue-600"
+            />
+            <span className="text-blue-600">{trait.title}</span>&nbsp;
+          </dt>
 
-        <dd className="inline text-gray-600">
-          Nascido em Roraima e criado no Piauí, onde construí parte da minha
-          vida. Morei lá até os meus 27 anos.
-        </dd>
-      </div>
-      <div className="relative pl-9">
-        <dt className="inline font-semibold text-gray-900">
-          <svg
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className="absolute left-1 top-1 h-6 w-6 text-blue-600"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M10.5 21l5.25-11.25L21 21m-9-3h7.5M3 5.621a48.474 48.474 0 016-.371m0 0c1.12 0 2.233.038 3.334.114M9 5.25V3m3.334 2.364C11.176 10.658 7.69 15.08 3 17.502m9.334-12.138c.896.061 1.785.147 2.666.257m-4.589 8.495a18.023 18.023 0 01-3.827-5.802"
-            ></path>
-          </svg>
-          Poliglota.
-        </dt>
-
-        <dd className="inline text-gray-600">
-          Além do Português, também entendo e falo em Inglês (C2), Espanhol
-          (B1), e Alemão (A2/B1).
-        </dd>
-      </div>
-      <div className="relative pl-9">
-        <dt className="inline font-semibold text-gray-900">
-          <svg
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className="absolute left-1 top-1 h-6 w-6 text-blue-600"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M9.813 15.904L9 18.75l-.813-2.846a4.5 4.5 0 00-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 003.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 003.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 00-3.09 3.09zM18.259 8.715L18 9.75l-.259-1.035a3.375 3.375 0 00-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 002.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 002.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 00-2.456 2.456zM16.894 20.567L16.5 21.75l-.394-1.183a2.25 2.25 0 00-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 001.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 001.423 1.423l1.183.394-1.183.394a2.25 2.25 0 00-1.423 1.423z"
-            ></path>
-          </svg>
-          20 anos de experiência.
-        </dt>
-
-        <dd className="inline text-gray-600">
-          Comecei na época do Frontpage. Foquei meus trabalhos em sistemas web e
-          mobile.
-        </dd>
-      </div>
-      <div className="relative pl-9">
-        <dt className="inline font-semibold text-gray-900">
-          <svg
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className="absolute left-1 top-1 h-6 w-6 text-blue-600"
-            aria-hidden="true"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M4.26 10.147a60.436 60.436 0 00-.491 6.347A48.627 48.627 0 0112 20.904a48.627 48.627 0 018.232-4.41 60.46 60.46 0 00-.491-6.347m-15.482 0a50.57 50.57 0 00-2.658-.813A59.905 59.905 0 0112 3.493a59.902 59.902 0 0110.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.697 50.697 0 0112 13.489a50.702 50.702 0 017.74-3.342M6.75 15a.75.75 0 100-1.5.75.75 0 000 1.5zm0 0v-3.675A55.378 55.378 0 0112 8.443m-7.007 11.55A5.981 5.981 0 006.75 15.75v-1.5"
-            ></path>
-          </svg>
-          Cientista da Computação.
-        </dt>
-
-        <dd className="inline text-gray-600">
-          Formado pela Universidade Estadual do Piauí — UESPI, com muito
-          orgulho.
-        </dd>
-      </div>
-      <div className="relative pl-9">
-        <dt className="inline font-semibold text-gray-900">
-          <svg
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className="absolute left-1 top-1 h-6 w-6 text-blue-600"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
-            ></path>
-          </svg>
-          eCommerce.
-        </dt>
-
-        <dd className="inline text-gray-600">
-          Minha carreira na Europa é 100% voltada para criação de grandes lojas
-          online internacionais.
-        </dd>
-      </div>
-      <div className="relative pl-9">
-        <dt className="inline font-semibold text-gray-900">
-          <svg
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            className="absolute left-1 top-1 h-6 w-6 text-blue-600"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M14.25 6.087c0-.355.186-.676.401-.959.221-.29.349-.634.349-1.003 0-1.036-1.007-1.875-2.25-1.875s-2.25.84-2.25 1.875c0 .369.128.713.349 1.003.215.283.401.604.401.959v0a.64.64 0 01-.657.643 48.39 48.39 0 01-4.163-.3c.186 1.613.293 3.25.315 4.907a.656.656 0 01-.658.663v0c-.355 0-.676-.186-.959-.401a1.647 1.647 0 00-1.003-.349c-1.036 0-1.875 1.007-1.875 2.25s.84 2.25 1.875 2.25c.369 0 .713-.128 1.003-.349.283-.215.604-.401.959-.401v0c.31 0 .555.26.532.57a48.039 48.039 0 01-.642 5.056c1.518.19 3.058.309 4.616.354a.64.64 0 00.657-.643v0c0-.355-.186-.676-.401-.959a1.647 1.647 0 01-.349-1.003c0-1.035 1.008-1.875 2.25-1.875 1.243 0 2.25.84 2.25 1.875 0 .369-.128.713-.349 1.003-.215.283-.4.604-.4.959v0c0 .333.277.599.61.58a48.1 48.1 0 005.427-.63 48.05 48.05 0 00.582-4.717.532.532 0 00-.533-.57v0c-.355 0-.676.186-.959.401-.29.221-.634.349-1.003.349-1.035 0-1.875-1.007-1.875-2.25s.84-2.25 1.875-2.25c.37 0 .713.128 1.003.349.283.215.604.401.96.401v0a.656.656 0 00.658-.663 48.422 48.422 0 00-.37-5.36c-1.886.342-3.81.574-5.766.689a.578.578 0 01-.61-.58v0z"
-            ></path>
-          </svg>
-          Arquitetura de Sistemas.
-        </dt>
-
-        <dd className="inline text-gray-600">
-          Tenho trabalhado mais na arquitetura e modelagem de sistemas que em
-          programação.
-        </dd>
-      </div>
+          <dd className="inline text-gray-600">{trait.description}</dd>
+        </div>
+      ))}
     </dl>
   );
 }
