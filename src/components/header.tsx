7a6e69c9211cbad7092d8224efb421b925da9d65
erick patrick <github@erickpatrick.net>
2026-02-05 17:07:16 +0100

    adds locale to the link used in the logo

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,7 +5,7 @@ import { Locale } from "@/i18n-config";
 export default function Header({ locale }: { locale: Locale }) {
   return (
     <div className="p-4 flex justify-between align-center">
-      <Link href="/" className="group">
+      <Link href={`/${locale}`} className="group">
         <div className="flex justify-center">
           <div className="flex flex-col text-4xl font-black uppercase origin-center -rotate-6 border-4 border-gray-200 group-hover:border-white transition duration-100 ease-out">
             <h1 className="p-2 bg-transparent text-gray-800 rotate-6 group-hover:bg-gray-800 group-hover:text-white transition duration-200 ease-out delay-100">
