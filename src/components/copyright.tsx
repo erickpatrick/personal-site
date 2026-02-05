600456ee6155f5b871e813888fa18cad6ac2bb01
erick patrick <github@erickpatrick.net>
2026-02-05 10:34:05 +0100

    adds client component to automatically load the new year

---
+++ src/components/copyright.tsx
@@ -0,0 +1,8 @@
+"use client";
+export default function Copyright() {
+  return (
+    <p className="mt-10 text-center text-xs leading-5 text-gray-500">
+      &copy; {new Date().getFullYear()} Erick Patrick. All rights reserved.
+    </p>
+  );
+}
