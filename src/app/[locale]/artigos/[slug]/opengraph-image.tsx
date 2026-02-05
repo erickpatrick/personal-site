75a03fae4d310f0efe0a4f5ef997c9e4aef50bd3
erick patrick <github@erickpatrick.net>
2026-02-05 21:13:56 +0100

    fixes opengraph image generation

--- src/app/[locale]/artigos/[slug]/opengraph-image.tsx
+++ src/app/[locale]/artigos/[slug]/opengraph-image.tsx
@@ -14,30 +14,95 @@ export default async function Image({
 }: {
   params: { slug: string; locale: Locale };
 }) {
-  const post = getArticleBySlug(params.locale, params.slug);
+  const { locale, slug } = await params;
+  const post = getArticleBySlug(locale, slug);
 
   return new ImageResponse(
     <div
       style={{
-        background: "white",
-        width: "100%",
-        height: "100%",
         display: "flex",
-        flexDirection: "column",
+        height: "100%",
+        width: "100%",
+        alignItems: "center",
+        justifyContent: "center",
+        letterSpacing: "-.02em",
+        fontWeight: 700,
+        background: "white",
       }}
     >
-      <p
+      <div
+        style={{
+          position: "absolute",
+          top: 15,
+          left: 20,
+          height: 50,
+          width: 50,
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            transformOrigin: "center",
+            transform: "rotate(-6deg)",
+            border: "4px solid #e5e7eb",
+          }}
+        >
+          <h1
+            style={{
+              marginTop: -4,
+              marginLeft: 2,
+              padding: "4px",
+              backgroundColor: "transparent",
+              color: "#1e2939",
+              transform: "rotate(6deg)",
+              fontWeight: 900,
+            }}
+          >
+            EP
+          </h1>
+        </div>
+        <span style={{ padding: 12 }}>ErickPatrick.net</span>
+      </div>
+      <div
         style={{
           display: "flex",
-          backgroundColor: "black",
-          color: "white",
-          padding: "12px 16px",
-          fontSize: 48,
+          flexWrap: "wrap",
+          justifyContent: "center",
+          padding: "20px 50px",
+          margin: "0 42px",
+          fontSize: 40,
+          width: "auto",
+          maxWidth: 550,
+          textAlign: "center",
+          color: "black",
+          lineHeight: 1.4,
+          textDecoration: "underline",
+          textDecorationColor: "#155dfc",
+          textDecorationStyle: "dotted",
+          textDecorationThickness: 4,
+          textUnderlineOffset: 16,
         }}
       >
         {post.title}
-      </p>
-      <p style={{ fontSize: "24" }}>{post.excerpt}</p>
+      </div>
+      <span
+        style={{
+          position: "absolute",
+          display: "flex",
+          justifyContent: "center",
+          bottom: 15,
+          left: 10,
+          right: 10,
+          fontSize: 12,
+          padding: 8,
+        }}
+      >
+        https://www.erickpatrick.net/{locale}/{post.slug}
+      </span>
     </div>,
     {
       ...size,
